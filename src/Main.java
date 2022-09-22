037636919e31b94c6a245e0801543384c2ef91e6
Nchaaso31 <josea.nchasobitorosa@iesf.onmicrosoft.com>
2022-09-22 12:29:53 +0200

    join de todoslos hilos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Accumulator accumulator = new Accumulator(0);
+        List<Thread> threadList = new LinkedList<>();
+//      CREAR LISTA DE HILOS
+        for (int i = 0; i < 100; i++){
+            Thread incrementalTHread = new Thread(new IncrementalTask(accumulator, 1000));
+            incrementalTHread.start();
+        }
+        try {
+//            HACER JOIN DE TODS LOS HILOS DE LA LISTA
 
-        System.out.println("Hello world!");
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(accumulator.getValue());
     }
 }
